027136b261fce8f20d770788bdabc5c710b809ba
Olivier Ligot <oligot@protonmail.com>
2025-07-20 12:20:17 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+          pkgs.awscli2
+          pkgs.bat
+          pkgs.delta
+          pkgs.difftastic
+          pkgs.direnv
+          pkgs.eza
+          pkgs.fzf
+          pkgs.go
+          pkgs.kubectl
+          (pkgs.lima.override {
+            withAdditionalGuestAgents = true;
+          })
+          pkgs.neovim
+          pkgs.nodejs_22
+          pkgs.nil
+          pkgs.pay-respects
+          pkgs.ripgrep
+          pkgs.tig
+          pkgs.tmux
+          pkgs.uv
+          pkgs.zoxide
+          pkgs.zplug
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      security.pam.services.sudo_local.touchIdAuth = true;
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Oliviers-MacBook-Air
+    darwinConfigurations."Oliviers-MacBook-Air" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# macosinstall
+
+>  Install scripts to setup my development environment on MacOS
+
+## Terminal
+
+[Download and install Ghostty](https://ghostty.org/download)
+
+## Nix
+
+### Install
+
+```bash
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+sudo nix run nix-darwin -- switch --flake .
+```
+
+### Update
+
+```bash
+nix flake update --commit-lock-file
+sudo darwin-rebuild switch --flake .
+```
+
+## Tools
+
+### Tmux Plugin Manager
+
+```bash
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+```
+
+## dotfiles
+
+```bash
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply oligot
+```
