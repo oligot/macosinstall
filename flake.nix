0c1f149ec0970a07c27901d8e90b2aecfafabd76
Olivier Ligot <oligot@protonmail.com>
2025-07-25 18:12:19 +0200

    feat: install LSP servers

--- flake.nix
+++ flake.nix
@@ -7,61 +7,76 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.awscli2
-          pkgs.bat
-          pkgs.delta
-          pkgs.difftastic
-          pkgs.direnv
-          pkgs.eza
-          pkgs.fzf
-          pkgs.go
-          pkgs.kubectl
-          (pkgs.lima.override {
-            withAdditionalGuestAgents = true;
-          })
-          pkgs.neovim
-          pkgs.nodejs_22
-          pkgs.nil
-          pkgs.pay-respects
-          pkgs.ripgrep
-          pkgs.tig
-          pkgs.tmux
-          pkgs.uv
-          pkgs.zoxide
-          pkgs.zplug
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.awscli2
+            pkgs.bash-language-server
+            pkgs.bat
+            pkgs.delta
+            pkgs.difftastic
+            pkgs.direnv
+            pkgs.eza
+            pkgs.fd
+            pkgs.fzf
+            pkgs.go
+            pkgs.kubectl
+            (pkgs.lima.override {
+              withAdditionalGuestAgents = true;
+            })
+            pkgs.lua-language-server
+            pkgs.marksman
+            pkgs.neovim
+            pkgs.nodejs_22
+            pkgs.nil
+            pkgs.nixfmt-rfc-style
+            pkgs.pay-respects
+            pkgs.ripgrep
+            pkgs.stylua
+            pkgs.tig
+            pkgs.tmux
+            pkgs.tree-sitter
+            pkgs.uv
+            pkgs.vscode-langservers-extracted
+            pkgs.yaml-language-server
+            pkgs.zoxide
+            pkgs.zplug
+          ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
 
-      security.pam.services.sudo_local.touchIdAuth = true;
+          security.pam.services.sudo_local.touchIdAuth = true;
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Oliviers-MacBook-Air
+      darwinConfigurations."Oliviers-MacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Oliviers-MacBook-Air
-    darwinConfigurations."Oliviers-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
