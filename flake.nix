434bd9bae8f1770ae0f4e8c9dfaf08d7e7d1e7a8
Olivier Ligot <oligot@protonmail.com>
2025-12-30 17:32:26 +0100

    feat: add some more tools (mainly k8s oriented)
    also alias docker to podman

--- flake.nix
+++ flake.nix
@@ -21,47 +21,75 @@
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
             pkgs.aerospace
+            pkgs.argocd
+            pkgs.aspell
+            pkgs.aspellDicts.fr
             pkgs.awscli2
             pkgs.bash-language-server
             pkgs.bat
             pkgs.delta
+            pkgs.devpod
             pkgs.difftastic
             pkgs.direnv
+            # pkgs.docker
             pkgs.eza
             pkgs.fd
             pkgs.fzf
             pkgs.gh
             pkgs.git
+            pkgs.git-extras
+            pkgs.glab
             pkgs.go
+            pkgs.helm-ls
+            pkgs.himalaya
+            pkgs.hl-log-viewer
             pkgs.k9s
             pkgs.kanata
+            pkgs.kube-capacity
+            pkgs.kubecm
+            pkgs.kubectx
             pkgs.kubectl
+            pkgs.kubernetes-helm
             pkgs.librewolf
             (pkgs.lima.override {
               withAdditionalGuestAgents = true;
             })
+            pkgs.kustomize
             pkgs.lua-language-server
             pkgs.marksman
             pkgs.neovim
             pkgs.nil
             pkgs.nixfmt-rfc-style
+            pkgs.nova
             pkgs.ov
             pkgs.overmind
             pkgs.pay-respects
+            pkgs.podman
+            pkgs.podman-compose
             pkgs.postgresql
             pkgs.prettier
+            pkgs.python313Packages.cfn-lint
+            pkgs.ranger
             pkgs.ripgrep
+            pkgs.saml2aws
             pkgs.stylua
+            pkgs.taplo
             pkgs.tig
             pkgs.tldr
             pkgs.tmux
             pkgs.tokei
             pkgs.tree-sitter
-            pkgs.uv
+            # pkgs.uv
             pkgs.vscode-langservers-extracted
+            pkgs.yamlfmt
             pkgs.yaml-language-server
+            pkgs.yq-go
             pkgs.zoxide
             pkgs.zplug
+
+            (pkgs.writeShellScriptBin "docker" ''
+              exec ${pkgs.podman}/bin/podman "$@"
+            '')
           ];
 
           # Necessary for using flakes on this system.
